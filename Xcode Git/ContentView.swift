bf126533084f1c1296bb0f9f4296c02b87596e87
MarcCDJ <marccruzdj@gmail.com>
2023-07-29 12:55:58 -0400

    Updated ContentView

--- Xcode Git/ContentView.swift
+++ Xcode Git/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             NavigationLink(destination: ModalView()) {
                 Text("Go Next")
             }
-            .foregroundColor(.purple)
+            .foregroundColor(.blue)
             .navigationBarTitle("Xcode and Git")
         }
     }
