62bacbf0ead1a3ece50554388c7dea139c4ca1a2
MarcCDJ <marccruzdj@gmail.com>
2023-07-29 08:25:19 -0400

    Added modal view

---
+++ Xcode Git/ModalView.swift
@@ -0,0 +1,18 @@
+//
+//  ModalView.swift
+//  Xcode Git
+//
+//  Created by Marc Cruz on 7/29/23.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text("This is the modal view!")
+    }
+}
+
+#Preview {
+    ModalView()
+}
