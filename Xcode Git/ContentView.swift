cf77d67fdefbccf168a3876c0e5c7f99aaceaace
MarcCDJ <marccruzdj@gmail.com>
2023-07-29 08:05:09 -0400

    Initial commit

--- Xcode Git/ContentView.swift
+++ Xcode Git/ContentView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPresented = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            Button("Show Modal") {
+                self.isPresented = true
+            }.sheet(isPresented: $isPresented) {
+                Text("This is the modal screen")
+            }
         }
-        .padding()
     }
 }
 
