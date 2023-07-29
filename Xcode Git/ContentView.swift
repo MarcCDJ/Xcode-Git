e591b4bf7828509358ceaaad5a988802b22d993d
MarcCDJ <marccruzdj@gmail.com>
2023-07-29 07:55:53 -0400

    Initial Commit

---
+++ Xcode Git/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Xcode Git
+//
+//  Created by Marc Cruz on 7/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Xcode Git/Xcode_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_GitApp.swift
+//  Xcode Git
+//
+//  Created by Marc Cruz on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
